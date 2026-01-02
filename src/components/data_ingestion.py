08c7f637d9dea500785bee1bd91778c4e1e4220c
O-ASwIN-O <official.maswin@gmail.com>
2026-01-02 21:27:09 +0530

    Data Ingestion & Creation of artifacts folder

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import sys 
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -14,7 +14,7 @@ from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv") #path where train data will be stored
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
@@ -25,17 +25,17 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv') # we can replace this with any data source
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)# create the artifacts folder if not present
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) #save the raw data in artifacts folder
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) #save the train data in artifacts folder
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
@@ -51,13 +51,6 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-
+    obj.initiate_data_ingestion()
 
 
