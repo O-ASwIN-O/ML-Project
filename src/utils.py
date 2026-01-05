82152dd936cc3fba8ce7b2f7d6d64cb940fa5697
O-ASwIN-O <official.maswin@gmail.com>
2026-01-05 12:02:34 +0530

    Model Trainer Done

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score #used for evaluating regression models
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
